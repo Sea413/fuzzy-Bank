a5de68dc0fa4288eb88358a4c48a77edd1ab36b6
Abigail Rolling and Sean Peerenboom <arolling@gmail.com>
2016-01-26 11:55:51 -0800

    Add basic front end

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,45 @@ BankAccount.prototype.transaction = function(addedValue) {
 //   this.balance = parseFloat(this.balance.toPrecision(12));
 //   return this.balance;
 // }
+
+$(document).ready(function() {
+
+
+  $("form.test").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedInitialDeposit = parseFloat($("input#initialdeposit").val());
+    if (Math.sign(inputtedInitialDeposit) === 1) {
+      var newAccount = new BankAccount(inputtedFirstName, inputtedLastName, inputtedInitialDeposit);
+      console.log(newAccount);
+      $("span#errormsg").hide();
+    } else {
+      $("span#errormsg").html("<h2>Please enter a positive number for your initial deposit</h2>");
+    }
+    // $(".new-address").each(function() {
+    //   var inputtedStreet = $(this).find("input.new-street").val();
+    //   var inputtedCity = $(this).find("input.new-city").val();
+    //   var inputtedState = $(this).find("input.new-state").val();
+    //   var inputtedCategory = $(this).find("input.new-category").val();
+    //   var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedCategory);
+    //   newContact.addresses.push(newAddress);
+    // })
+    //
+    // $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    //
+    // $(".contact").last().click(function() {
+    //   $("#show-contact").show();
+    //   $("#show-contact h2").text(newContact.fullName());
+    //   $(".first-name").text(newContact.firstName);
+    //   $(".last-name").text(newContact.lastName);
+    //   $("ul#addresses").text('');
+    //   newContact.addresses.forEach(function(address) {
+    //     $("ul#addresses").append('<strong>' + address.category + ':</strong><li>' + address.fullAddress() +'</li>');
+    //   });
+    // });
+    // clearFields();
+    // $(".new-address").not(":first").remove();
+  });
+}); // END DOCUMENT READY
